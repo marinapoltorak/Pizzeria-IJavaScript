af04bc291195125ae12707aec90d3ff410fc6a57
Marina Poltorak <emylisfamily@gmail.com>
2019-06-21 15:38:50 -0700

    fix id errors and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,95 +3,67 @@
 function Pizza(size, toppings, price) {
   this.size = size,
   this.toppings = [],
-  this.price = price;
+  this.price = 0;
 }
 
 //this will add toppings to the pizza
-Pizza.prototype.addToppings = function () {
-  this.toppings.push(topping);
+Pizza.prototype.addTopping = function (inputtedTopping1, inputtedTopping2, inputtedTopping3) {
+  if (inputtedTopping1 !== '') {
+    this.toppings.push(inputtedTopping1 + ',' + ' ');
+  }
+
+  if (inputtedTopping2 !== '') {
+    this.toppings.push(inputtedTopping2 + ',' + ' ');
+  }
+
+  if (inputtedTopping3 !== '') {
+    this.toppings.push(inputtedTopping3 + ',' + ' ');
+  }
 };
 
 //this will add price to the pizza
 Pizza.prototype.addPrice = function () {
-
-  //business logic for toppings--
-  function Topping(topping1, topping2, topping3) {
-    this.topping1 = topping1,
-    this.topping2 = topping2,
-    this.topping3 = topping3;
-  }
-
-  Topping.prototype.toppingString = function () {
-    return this.topping1 + ', ' + this.topping2 + ' and ' + this.topping3;
-  };
-
-  //business logic for addPrice
-  function Price(price) {
-    this.price = price;
-  }
-
-  Price.prototype.addUpPrice = function () {
-    if (this.size === 'small') {
-      this.price = 10;
-      this.price += this.toppings.length;
-    }else if (this.size === 'medium') {
-      this.price = 15;
-      this.price += (this.toppings.length * 2);
-      console.log(this.price);
-      console.log(this.toppings.length);
-    }else if (this.size === 'large') {
-      this.price = 20;
-      this.price += (this.toppings.length * 3);
-      console.log(this.price);
-      console.log(this.toppings.length);
-    }
-
+  if (this.size === 'small') {
+    this.price = 6;
+    this.price += (this.toppings.length);
+  } else if (this.size === 'medium') {
+    this.price = 8;
+    this.price += (this.toppings.length * 2);
+    console.log(this.price);
+    console.log(this.toppings.length);
+  } else if (this.size === 'large') {
+    this.price = 10;
+    this.price += (this.toppings.length * 3);
     return this.price;
-  };
+  }
 };
 
 //user interface goes here
-var yourPizza = new Pizza();
-
-function displayPizzaDetails(pizzaToDisplay) {
-  var toppingsList = $('ul#your-pizza');
-  var htmlForPizzaDetails = '';
-  pizzaToDisplay.toppings.forEach(function(topping) {
-    htmlForPizzaDetails += '<li' + this.toppings + '>' + toppings.topping1 + ', ' + topping.topping2 + ', and '
-    toppings.topping3 + '</li>';
-  })
+function yourPizza(size, toppings, price) {
+  var toppingsString = toppings;
+  $('form#new-order').hide();
+  $('#your-pizza').show();
+  $('#show-size').append(size);
+  $('#show-price').append(price);
+  if (toppings.length > 0) {
+    $('#show-toppings').show();
+    $('#show-toppings').append(String(toppingsString.join('').slice(0, -2)));
+  }
 }
 
 $(document).ready(function () {
-
-  $('form#new-order').submit(function (event) {
-    event.preventDefault();
-    var inputSize = $('select.new-pizza-size').val();
-    var inputTopping1 = $('select.new-inputTopping1').val();
-    var inputTopping2 = $('select.new-inputTopping2').val();
-    var inputTopping3 = $('select.new-inputTopping3').val();
+    $('form#new-order').submit(function (event) {
+      event.preventDefault();
+      var inputtedSize = $('select.new-pizza-size').val();
+      var inputtedTopping1 = $('select.new-topping1').val();
+      var inputtedTopping2 = $('select.new-topping2').val();
+      var inputtedTopping3 = $('select.new-topping3').val();
+      var myPizza = new Pizza(inputtedSize, inputtedTopping1, inputtedTopping2,
+      inputtedTopping3, 0);
+      myPizza.addTopping(inputtedTopping1, inputtedTopping2, inputtedTopping3);
+      myPizza.addPrice();
+      yourPizza(myPizza.size, myPizza.toppings, myPizza.price);
+      return myPizza;
+      console.log(myPizza);
+    });
   });
-
-  //create an instanse of Pizza from inputs
-
-  yourPizza.addToppings(inputTopping1, inputTopping2, inputTopping3);
-  yourPizza.addPrice(price);
-
-  // YourPizza(yourPizza.size, yourPizza.toppings,  yourPizza.price);
-  console.log(yourPizza);
-  console.log(yourPizza);
-
-  //this will construct pizza order and show size and price
-  function YourPizza(size, toppings, price) {
-    var toppingsString = toppings;
-    $('form#new-order').hide();
-    $('#your-pie').show();
-    $('show-size').append(size);
-    $('show-price').append(price);
-    if (toppings.length > 0) {
-      $('#show-toppings').show();
-      $('#show-toppings').append(String(toppings.join('').slice(0, -2)));
-      console.log(yourPizza);
-    }
-  }
-});
